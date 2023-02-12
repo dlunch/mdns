1a3867fb3b48e9a42426d51dcc8b21adf7561175
Inseok Lee <git@dlun.ch>
2023-02-12 16:13:18 +0900

    Logger to example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,5 +1,7 @@
 #[tokio::main]
 pub async fn main() {
+    let _ = pretty_env_logger::try_init();
+
     let service = simple_mdns::Service::new("test", "test", 1234, vec!["testtest"]);
     let server = simple_mdns::Server::new(vec![service]).unwrap();
     server.serve().await.unwrap();
